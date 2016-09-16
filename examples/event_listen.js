ad6b8a1f4b2aef382cfd98f50cffc3087e41c77a
Remington Campbell <remcampb@cisco.com>
2016-09-15 18:51:25 -0700

    XRCL-18 Callbacks -> Event Listening
    Wrap gRPC callbacks as event driven and maintain consistent access to
    data. This may or may not be a good way to go about it and needs
    further evaluation.

--- examples/event_listen.js
+++ examples/event_listen.js
@@ -1,14 +1,46 @@
-var xrGrpc = require('../../lib/index.js');
+/**
+ * Simple/quick example of merge then deletion of configuration.
+ * Show configuration before/after each step.
+ * Meant to be used from test environment from /xrgrpc/examples/
+ */
+
+var xrGrpc = require('../lib/index.js');
 
 function main() {
+
 	var metadata = {
 		username: 'vagrant',
 		password: 'vagrant'
 	};
-
 	var yangpath = '{"Cisco-IOS-XR-ip-static-cfg:router-static": [null]}';
 	var client = xrGrpc('11.1.1.254', 57500, null, metadata, null);
-	var stream = client.getConfig(0, yangpath, null);
+
+	console.log('\n\nORIGINAL CONFIG\n\n');
+	showGetConfig(client, yangpath);
+	
+	setTimeout(function() {
+		console.log('\n\nMERGING STATIC ROUTES IN\n\n');
+		mergeConfig(client, '{"Cisco-IOS-XR-ip-static-cfg:router-static": {"default-vrf": {"address-family": {"vrfipv4": {"vrf-unicast": {"vrf-prefixes": {"vrf-prefix": [{"prefix": "1.2.3.6", "vrf-route": {"vrf-next-hop-table": {"vrf-next-hop-next-hop-address": [{"next-hop-address": "10.0.2.2"}]}}, "prefix-length": 32}]}}}}}}}');
+	}, 1000);
+
+	setTimeout(function() {
+		console.log('\n\nNEW CONFIG\n\n');
+		showGetConfig(client, yangpath);
+	}, 2000);
+
+	setTimeout(function() {
+		console.log('\n\nDELETING STATIC ROUTES\n\n');
+		deleteConfig(client, '{"Cisco-IOS-XR-ip-static-cfg:router-static": {"default-vrf": {"address-family": {"vrfipv4": {"vrf-unicast": {"vrf-prefixes": {"vrf-prefix": [{"prefix": "1.2.3.6", "vrf-route": {"vrf-next-hop-table": {"vrf-next-hop-next-hop-address": [{"next-hop-address": "10.0.2.2"}]}}, "prefix-length": 32}]}}}}}}}');
+	}, 3000);
+
+	setTimeout(function() {
+		console.log('\n\nFINAL CONFIG\n\n');
+		showGetConfig(client, yangpath);
+	}, 4000);
+}
+
+function showGetConfig(client, yangpath) {
+	var stream = client.getConfig(0, yangpath);
 
 	stream.on('data', function(data) {
 		data = data.toObject();
@@ -29,6 +61,26 @@ function main() {
 			console.log('No details.');
 		else
 			console.log(status.details);
+	});
+}
+
+function deleteConfig(client, yangJSON) {
+	var command = client.deleteConfig(0, yangJSON);
+	command.on('data', function(data) {
+		console.log(data);
+	});
+	command.on('error', function(err) {
+		console.log(err);
+	});
+}
+
+function mergeConfig(client, yangJSON) {
+	var command = client.mergeConfig(0, yangJSON);
+	command.on('data', function(data) {
+		console.log(data);
+	});
+	command.on('error', function(err) {
+		console.log(err);
 	});
 }
 
